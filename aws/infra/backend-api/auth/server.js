deb2e7da2b5c18eaf83d01b1f3f24ad85c4c3fa9
ferreiraalexandre <alexandreferreira.xande@gmail.com>
2025-11-02 20:26:38 -0300

    Change endpoint from '/' to '/auth'

--- aws/infra/backend-api/auth/server.js
+++ aws/infra/backend-api/auth/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3002;
 
-app.get('/', (req, res) => {
+app.get('/auth', (req, res) => {
   res.send('API de autenticação respondendo na porta 3002');
 });
 
